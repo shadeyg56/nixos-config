53d4b23908abdd3695ff2486af1da6b7bc0d8572
shadeyg56 <shadeyg56@gmail.com>
2023-08-09 00:48:22 -0500

    fix this terrible zsh mess

--- terminal/zsh/default.nix
+++ terminal/zsh/default.nix
@@ -2,7 +2,31 @@
 {
     programs.zsh = {
         enable = true;
-        #dotDir = "nixos-config/terminal/zsh";
-        initExtra = "source $HOME/nixos-config/terminal/zsh/.zshrc";
+        enableSyntaxHighlighting = true;
+        plugins = [
+            {
+                name = "powerlevel10k";
+                src = pkgs.zsh-powerlevel10k;
+                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+            {
+                file = "p10k.zsh";
+                name = "powerlevel10k-config";
+                src = ../zsh;
+            }
+            {
+                name = "zsh-autosuggestions";
+                src = pkgs.zsh-autosuggestions;
+                file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+            }
+        ];
+        #initExtra = "source $HOME/nixos-config/terminal/zsh/.zshrc";
     };
+
+    # themes, and plugins
+    home.packages = with pkgs; [
+        zsh-powerlevel10k
+        zsh-autosuggestions
+        zsh-syntax-highlighting
+    ];
 }
