d116fc025c8c2a328e98f9bc9e20b456a6b75109
shadeyg56 <shadeyg56@gmail.com>
2023-08-11 18:56:08 -0500

    add spicetify and refactor file

--- flake.nix
+++ flake.nix
@@ -23,106 +23,35 @@
     };
     hyprland.url = "github:hyprwm/Hyprland";
 
+    spicetify-nix.url = "github:the-argus/spicetify-nix";
+
     sddm-sugar-candy-nix = {
       url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  outputs = { self, nixpkgs, home-manager, hyprland, sddm-sugar-candy-nix, ... }@inputs: 
+  let 
+    system = "x86_64-linux";
 
-  # `outputs` are all the build result of the flake.
-  #
-  # A flake can have many use cases and different types of outputs.
-  # 
-  # parameters in function `outputs` are defined in `inputs` and
-  # can be referenced by their names. However, `self` is an exception,
-  # this special parameter points to the `outputs` itself(self-reference)
-  # 
-  # The `@` syntax here is used to alias the attribute set of the
-  # inputs's parameter, making it convenient to use inside the function.
-  outputs = { self, nixpkgs, home-manager, hyprland, sddm-sugar-candy-nix, ... }@inputs: {
+    pkgs = nixpkgs.legacyPackages.${system};
 
-  #  homeConfigurations = {
- #       shadeyg56 = home-manager.lib.homeManagerConfiguration {
-#	    modules = [
-#		/home/shadeyg56/home.nix
-#	    ];
-#	};
-    #};
-
+  in {
     nixosConfigurations = {
-      # By default, NixOS will try to refer the nixosConfiguration with
-      # its hostname, so the system named `nixos-test` will use this one.
-      # However, the configuration name can also be specified using:
-      #   sudo nixos-rebuild switch --flake /path/to/flakes/directory#<name>
-      #
-      # The `nixpkgs.lib.nixosSystem` function is used to build this
-      # configuration, the following attribute set is its parameter.
-      #
-      # Run the following command in the flake's directory to
-      # deploy this configuration on any NixOS system:
-      #   sudo nixos-rebuild switch --flake .#nixos-test
       "nixos" = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	specialArgs = { inherit inputs; };
-	modules = [
-	    ./configuration.nix
-	    hyprland.nixosModules.default
-	    home-manager.nixosModules.home-manager {
-		    home-manager.useGlobalPkgs = true;
-		    home-manager.useUserPackages = true;
-		    home-manager.extraSpecialArgs = { inherit inputs; };
-		    home-manager.users.shadeyg56 = import ./home.nix;
-	    }
-      sddm-sugar-candy-nix.nixosModules.default
-	];
-       };
-
-
-      "nixos-test" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        # The Nix module system can modularize configuration,
-        # improving the maintainability of configuration.
-        #
-        # Each parameter in the `modules` is a Nix Module, and
-        # there is a partial introduction to it in the nixpkgs manual:
-        #    <https://nixos.org/manual/nixpkgs/unstable/#module-system-introduction>
-        # It is said to be partial because the documentation is not
-        # complete, only some simple introductions.
-        # such is the current state of Nix documentation...
-        #
-        # A Nix Module can be an attribute set, or a function that
-        # returns an attribute set. By default, if a Nix Module is a
-        # function, this function can only have the following parameters:
-        #
-        #  lib:     the nixpkgs function library, which provides many
-        #             useful functions for operating Nix expressions:
-        #             https://nixos.org/manual/nixpkgs/stable/#id-1.4
-        #  config:  all config options of the current flake, every useful
-        #  options: all options defined in all NixOS Modules
-        #             in the current flake
-        #  pkgs:   a collection of all packages defined in nixpkgs,
-        #            plus a set of functions related to packaging.
-        #            you can assume its default value is
-        #            `nixpkgs.legacyPackages."${system}"` for now.
-        #            can be customed by `nixpkgs.pkgs` option
-        #  modulesPath: the default path of nixpkgs's modules folder,
-        #               used to import some extra modules from nixpkgs.
-        #               this parameter is rarely used,
-        #               you can ignore it for now.
-        #
-        # Only these parameters can be passed by default.
-        # If you need to pass other parameters,
-        # you must use `specialArgs` by uncomment the following line:
-        #
-        # specialArgs = {...}  # pass custom arguments into all sub module.
+        specialArgs = { inherit inputs; };
         modules = [
-          # Import the configuration.nix here, so that the
-          # old configuration file can still take effect.
-          # Note: configuration.nix itself is also a Nix Module,
-          ./configuration.nix
-        ];
-      };
+	        ./configuration.nix
+	        hyprland.nixosModules.default
+	        home-manager.nixosModules.home-manager {
+		        home-manager.useGlobalPkgs = true;
+		        home-manager.useUserPackages = true;
+		        home-manager.extraSpecialArgs = { inherit inputs; };
+		        home-manager.users.shadeyg56 = import ./home.nix;
+	        }
+          sddm-sugar-candy-nix.nixosModules.default
+	      ];
+       };
     };
   };
 }
