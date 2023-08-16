60b0e643634456d2437fdbc7b22fbe534f2a926f
shadeyg56 <shadeyg56@gmail.com>
2023-08-16 00:19:54 -0500

    spotifywm not working

--- programs/spicetify/default.nix
+++ programs/spicetify/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, ... }:
 let
     spicePkgs = inputs.spicetify-nix.packages.x86_64-linux.default;
 in {
@@ -6,13 +6,19 @@ in {
         inputs.spicetify-nix.homeManagerModule
     ];
 
+    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "spotify"
+     ];
+
     programs.spicetify = {
         enable = true;
         theme = spicePkgs.themes.catppuccin-macchiato;
         colorScheme = "lavender";
         enabledExtensions = with spicePkgs.extensions; [
             adblock
+            shuffle
         ];
+	#windowManagerPatch = true;
         #spotifyPackage = (pkgs.callPackage ../../pkgs/spotify-adblock.nix { });
     };
     
