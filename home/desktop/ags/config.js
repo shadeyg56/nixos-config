a0dc37171faf1f9683e662abe2035061722b986e
shadeyg56 <shadeyg56@gmail.com>
2024-04-19 01:14:16 -0500

    ags: create Calendar widget

--- home/desktop/ags/config.js
+++ home/desktop/ags/config.js
@@ -4,6 +4,7 @@ import App from 'resource:///com/github/Aylur/ags/app.js';
 import Bar from './js/bar/bar.js'
 import ControlCenter from './js/ControlCenter/ControlCenter.js';
 import MediaWindow from './js/MediaWindow/media.js';
+import Calendar from './js/Calendar/Calendar.js';
 
 let config = {
     style: App.configDir + '/style.css',
@@ -11,10 +12,12 @@ let config = {
         Bar(0),
         ControlCenter(),
         MediaWindow(),
+        Calendar()
     ],
     closeWindowDelay: {
         'controlcenter': 350,
         'media': 350,
+        'calendar': 350
       },
 };
 
