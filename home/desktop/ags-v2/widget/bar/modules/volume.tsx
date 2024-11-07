ed3ed89453c06e03cef0b2ac84595006561e19d3
shadeyg56 <shadeyg56@gmail.com>
2024-11-07 15:42:46 -0600

    ags: volume in bar

---
+++ home/desktop/ags-v2/widget/bar/modules/volume.tsx
@@ -0,0 +1,84 @@
+import { Widget, Gtk } from "astal/gtk3";
+import { bind, Binding, Variable } from "astal";
+import Wp from "gi://AstalWp";
+
+const audio = Wp.get_default()?.get_audio();
+
+function VolumeIcon() {
+
+    const volumeThresholds = [101, 67, 34, 1, 0];
+
+    const setupStack = (stack: Widget.Stack) => {
+        if (!audio)
+            return
+        audio.get_default_speaker()?.connect("notify", ((speaker) => {
+            if (speaker.get_mute()) {
+                stack.shown = "0";
+                return;
+            }
+
+            stack.shown = volumeThresholds.find((threshold) => threshold <= speaker.volume * 100)!.toString();
+        }))
+    }
+
+
+    return (
+        <box className="volIcon">
+            <stack setup={(setupStack)}>
+                <icon name="101" icon="audio-volume-overamplified-symbolic"/>
+                <icon name="67" icon="audio-volume-high-symbolic"/>
+                <icon name="34" icon="audio-volume-medium-symbolic"/>
+                <icon name="1" icon="audio-volume-low-symbolic"/>
+                <icon name="0" icon="audio-volume-muted-symbolic"/>
+            </stack>
+        </box>
+    )
+}
+
+interface RevealerProps {
+    revealChild: boolean | Binding<boolean>
+}
+
+function PercentBar({revealChild}: RevealerProps) {
+    return (
+        <revealer
+        transitionType={Gtk.RevealerTransitionType.SLIDE_RIGHT}
+        revealChild={revealChild}
+        >
+            <slider className="volBar"
+            hexpand={true}
+            drawValue={false}
+            value={bind(audio!.get_default_speaker()!, "volume")}
+            >
+
+            </slider>
+        </revealer>
+    )
+}
+
+export default function Volume() {
+
+    const revealChild = Variable(false);
+
+    return (
+        <eventbox
+        className="volume"
+        onHover={() => revealChild.set(true)}
+        onHoverLost={(widget, event) => {
+            const x = event.x;
+            const y = event.y;
+            const w = widget.get_allocation().width;
+            const h = widget.get_allocation().height;
+            if (x < 0 || x > w || y < 0 || y > h) {
+                revealChild.set(false);
+            }
+        }}
+        >
+            <box>
+                <VolumeIcon/>
+                <PercentBar revealChild={bind(revealChild)}/>
+            </box>
+        </eventbox>
+        
+    )
+}
